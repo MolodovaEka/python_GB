02a3c14ad43cf49b504029ae878a35b596813766
Eka_Eka <ekamolodova@gmail.com>
2023-05-27 02:08:06 -0700

    Plus sem_8 homework

--- sem_7/34.py
+++ sem_7/34.py
@@ -25,6 +25,25 @@ if count_vowels(my_phrases):
     print('Парам пам-пам')
 else:
     print('Пам парам')
+
+
+
+# def rythm(poem):
+#     syllables = [] # список с количеством слогов в каждой фразе
+#     for phrase in poem:
+#         vowels = [letter for letter in phrase if letter in 'аоиеёэыуюя'] # оставляем только гласные буквы
+#         # count_vowels = sum([1 for letter in phrase if letter in 'аоиеёэыуюя'])
+#         syllables.append(len(vowels)) # определяем число слогов и добавляем в список
+#     if len(set(syllables)) == 1: # если все числа в списке одинаковы
+#         return True
+#     return False
+
+# poem = input('Введите стихотворение ').split() 
+# # poem = 'пара-ра-рам рам-пам-папам па-ра-па-да'.split()
+# if rythm(poem):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам-парам')
         
     
     
